b97df4e3e38e915d8a6ba6bed76170c8c58faf45
Srodonxd <srodonxd@wp.pl>
2023-03-07 14:27:18 +0100

    git

---
+++ praktyki/7/ksiazki.py
@@ -0,0 +1,5 @@
+"""Napisz program, który będzie przechowywał informacje o książkach w bibliotece.
+ Utwórz klasę 'Book', która będzie miała atrybuty 'title', 'author' oraz 'isbn'. 
+ Następnie utwórz listę obiektów klasy 'Book', a w programie umożliw użytkownikowi dodawanie, usuwanie i wyświetlanie informacji o książkach z tej listy."""
+
+ 
