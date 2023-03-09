3ed7f7ae35267281e29e3bfd5fb40be9cd865000
Srodonxd <srodonxd@wp.pl>
2023-03-09 12:06:15 +0100

    git

--- praktyki/8/ksiazki.py
+++ praktyki/8/ksiazki.py
@@ -1,13 +1,11 @@
 """Napisz program, który będzie przechowywał informacje o książkach w bibliotece.
  Utwórz klasę 'Book', która będzie miała atrybuty 'title', 'author' oraz 'isbn'. 
  Następnie utwórz listę obiektów klasy 'Book', a w programie umożliw użytkownikowi dodawanie, usuwanie i wyświetlanie informacji o książkach z tej listy."""
-ilosc=0
-tytul=()
-autor=()
+
 class Book:
-    def __init__(self, book_list,nowe):
+    def __init__(self, book_list):
         self.book_list=book_list
-        self.nowe=nowe
+    
     def display_info(self):
         for book_number, book_info in book_list.items():
             print()
@@ -17,68 +15,84 @@ class Book:
             for attribute, value in book_info["Ilość"].items():
                 print(attribute + ":", value)
     
-    def dodaj(self):
+    def dodaj(self, nowe):
+        print(nowe)
+        ilosc=0
+        for item in book_list:
+            if(tytul in item and autor in item):
+                ilosc+=1
+            else:
+                self.book_list.append(nowe)
+                 
 
-       
-        if(tytul==book_list["Tytuł"] & autor==book_list["Autor"]):
-            book_list.update ({nowe()})
-        else:
-            ilosc+=numer
+        # to bedzie pod nowe   
+        #     nowe={
+
+        #         "Tytuł":tytul,
+        #         "Autor":autor,
+        #         "Ilość":{
+        #             "wypożyczone":0,
+        #             "dostępne":numer,
+        #             "razem":numer,
+        #         }      
+        # }
+        # Przeszukiwanie listy zeby znalexc czy taki tytul istnieje
+        
+        # Jesli istnieje update, jesli nie dddodac do listy slef.book_list.append(...)
+        # nie masz zmiennej tytul i autor, musisz to wziac z nowe['tytul'] i nowe['autor'] bo przekazujesz slownik
+
+
+        # Znajdx rozwiazanie w necie jak znalezc klucz slownika w liscie albo tak jak mowis zpoprostu petla w self.book_list i porownujesz tytuly
+
+            # Ilosc ? Czego, skad? To masz ilosc jakiejs zmiennej, no tak a jak juz masz ich 5? to dodasz o 1 do ilosci a w klasie nie zmieni sie wartosc tylko tam u gory
+            # potem znowu ktos dodaje i zamiast 2 ma 3 etc.
+            # wykonaj te instrukcje wyzej
             
 
 dzial=input("Co chcesz zrobić? \n 1)usunąć książke \n 2)dodać książke \n 3)zobaczyć tytuły \n 4)Wyjść \n")
 
+# TO jest list tytulow nie slownik tytulow jak miales
 
-book_list={
-        "Numer 1":{
-            "Tytuł":"IT",
-            "Autor":"nie pamiętam",
-            "Ilość":{
-                "wypożyczone":2, 
-                "dostępne":3, 
-                "razem":ilosc
-                    }
-                    },
-        "Numer 2":{
-            "Tytuł":"w pustyni i w puszczy", 
-            "Autor":"nie pamiętam", 
-            "Ilość":{
-                "wypożyczone":1, 
-                "dostępne":2, 
-                "razem":3
-                    }
-                    }, 
-        "Numer 3":{
-            "Tytuł":"Dziady", 
-            "Autor":"Mickiewicz",
-            "Ilość":{
-                "wypożyczone":1, 
-                "dostępne":2, 
-                "razem":3
-                    }
-                    }
-}
-nowe={}
-book=Book(book_list, nowe)
+book_list=[
+                {
+                "Tytuł":"IT",
+                "Autor":"nie pamiętam",
+                "Ilość":{
+                    "wypożyczone":2, 
+                    "dostępne":3, 
+                    "razem":5
+                        }
+                }, 
+                {
+                    "Tytuł":"w pustyni i w puszczy", 
+                    "Autor":"nie pamiętam", 
+                    "Ilość":{
+                        "wypożyczone":1, 
+                        "dostępne":2, 
+                        "razem":3
+                            }
+                }
+            ]
+
+book=Book(book_list)
 
 if(dzial=="2"):
     
-    for tytul, autor, numer in nowe():
-        tytul=input("Tytuł: ")
-        autor=input("Autor: ")
-        numer=input("Ile: ")
+    tytul=input("Tytuł: ")
+    autor=input("Autor: ")
+    numer=input("Ile: ")
     
     nowe={
-        f"Numer {ilosc}":{
-            "Tytuł":tytul,
-            "Autor":autor,
-            "Ilość":{
-                "wypożyczone":ilosc,
-                "dostępne":ilosc,
-                "razem":ilosc,
-            }
-        }       
+
+        "Tytuł":tytul,
+        "Autor":autor,
+        "Ilość":{
+            "wypożyczone":0,
+            "dostępne":numer,
+            "razem":numer,
+        }      
 }
+    book.dodaj(nowe)
 elif(dzial=="3"):
     book.display_info()
 
