00af22db8b62b568e108f269551f051b1bf30620
Srodonxd <srodonxd@wp.pl>
2023-03-10 13:59:49 +0100

    git

--- praktyki/9/lliczenie_slow.py
+++ praktyki/9/lliczenie_slow.py
@@ -7,4 +7,27 @@
 # Funkcja print_word_counts(word_counts: dict) -> None, która przyjmie jako argument słownik z ilością 
 # wystąpień słów i wypisze na ekranie listę słów wraz z ich ilością wystąpień, posortowaną alfabetycznie.
 
-def 
+count={}
+class Text:
+    def count_words(self, text_input):
+        split_text=str(text_input.split(" "))
+        print(split_text)
+        return
+
+    def print_words_counts(self, text_input):
+        for word in text_input():
+            if word in count:
+                count[word]+=1
+            else:
+                count[word]=1
+            
+
+for element, ilosc in count.items():
+    print(f"słowo {element} występuje {ilosc} razy")        
+
+
+text=Text()
+text_input=input("Wpisz tekst: ")
+
+text.count_words(text_input)
+text.print_words_counts(text_input)
