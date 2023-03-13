0313df65e04a1d84ef8e6735b081557a0c09b7dc
Srodonxd <srodonxd@wp.pl>
2023-03-13 12:31:49 +0100

    git

---
+++ praktyki/13/rusian_ruletka.py
@@ -0,0 +1,23 @@
+import random 
+x=money_totality=0
+list=[]
+group=int(input("Ile osób bierze udział?(MAKS 6 OSÓB): "))
+print("Podaj imiona oraz ich zakład")
+while x!=group:
+    new=input(f"{x+1}: ")
+    money=int(input("Ile PLN?: "))
+    list.append(new)
+    money_totality+=money
+    x+=1
+
+while group!=1:
+    death=random.randint(1,group)
+    while x!=7:
+        if x!=death:
+            print(f"Runda {x}: {list[death-1]}X")
+            list.pop(death-1)
+            break
+        else:
+            x+=1
+    group-=1
+print(f"Brawo! Zwycięscą jest {list[0]} i wygrywa {money_totality} PLN!!")
