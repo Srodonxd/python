ec39adcd47e2a81e7f2c3c01e92ccb6f344a0f91
Srodonxd <srodonxd@wp.pl>
2023-03-14 12:19:03 +0100

    git

--- praktyki/14/Transformacje_danych/ćwiczenie.py
+++ praktyki/14/Transformacje_danych/ćwiczenie.py
@@ -1,2 +1,10 @@
 #Znajdź liczby od 100 do 470 włącznie, zkóre są podzielne przez 7, ale nie są podzielne przez 5
 
+liczby=(
+    element 
+    for element in range(100,470)
+    if (element % 7 ==0)
+    if (element % 5 !=0)
+)
+
+print(liczby)
