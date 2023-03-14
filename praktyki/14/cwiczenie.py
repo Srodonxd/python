9fb18c3eaaed25db1cb24d83f94553ac89094dfe
Srodonxd <srodonxd@wp.pl>
2023-03-14 12:36:17 +0100

    git

--- praktyki/14/cwiczenie.py
+++ praktyki/14/cwiczenie.py
@@ -1,11 +1,18 @@
 #Znajdź liczby od 100 do 470 włącznie, zkóre są podzielne przez 7, ale nie są podzielne przez 5
 
+liczby=[
+    element 
+    for element in range(100,470)
+    if element % 7 ==0
+    if element % 5 !=0
+]
+print(liczby)
+
 liczby=(
     element 
     for element in range(100,470)
     if element % 7 ==0
     if element % 5 !=0
 )
-
 for liczba in liczby:
     print(liczba)
