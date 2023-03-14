a64caad3176414b6131116a04c871757bce952fd
Srodonxd <srodonxd@wp.pl>
2023-03-14 11:47:00 +0100

    git

---
+++ praktyki/14/Transformacje_danych/transformacje_danych.py
@@ -0,0 +1,44 @@
+import sys
+#Zapisuje dane w pamięci
+evenNumbers=[element
+             for element in range (400)
+             if (element % 2 == 0)
+             ]
+
+#Generuje dane
+evenNumbersGenerator=(element
+                      for element in range (400)
+                      if (element % 2 == 0)
+                      )
+
+print(sys.getsizeof(evenNumbers))
+print(sys.getsizeof(evenNumbersGenerator))
+
+
+
+
+names= {"Arkadiusz", "Wioletta", "Karol", "Bartłomiej", "Jakub", "Ania"}
+
+namesLenght = {
+    name : len(name)
+    for name in names
+    if name.startswith("A")
+}
+print(namesLenght)
+
+
+numbers=[1, 2, 3, 4, 5, 6]
+multipliednumbers = {
+    number : number*number
+    for number in numbers
+}
+print(multipliednumbers)
+
+celsius = {"t1":-20, "t2":-15, "t3":0, "t4":12, "t5":24}
+fahrenheit={
+    key:celsius*1.8 + 32
+    for key,celsius in celsius.items()
+    if celsius > -5
+    if celsius < 20
+}
+print(fahrenheit)
---
+++ praktyki/14/Transformacje_danych/ćwiczenie.py
@@ -0,0 +1,2 @@
+#Znajdź liczby od 100 do 470 włącznie, zkóre są podzielne przez 7, ale nie są podzielne przez 5
+
