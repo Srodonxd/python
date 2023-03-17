2aad8354bc2227586c4a0c79bfbad06d364ac892
Srodonxd <srodonxd@wp.pl>
2023-03-17 10:39:13 +0100

    git

---
+++ praktyki/16/sys_32.py
@@ -0,0 +1,24 @@
+import wmi
+
+w = wmi.WMI()
+
+for processor in w.Win32_Processor():
+    print("Procesor:")
+    print(f"- Nazwa: {processor.Name}")
+    print(f"- Ilość rdzeni: {processor.NumberOfCores}")
+    print(f"- Maksymalna częstotliwość: {processor.MaxClockSpeed} MHz")
+
+for gpu in w.Win32_VideoController():
+    print("Karta graficzna:")
+    print(f"- Nazwa: {gpu.Name}")
+    print(f"- Pamięć wideo: {gpu.AdapterRAM / 1024 / 1024:.2f} MB")
+
+for mem in w.Win32_PhysicalMemory():
+    print("Pamięć RAM:")
+    print(f"- Producent: {mem.Manufacturer}")
+    print(f"- Pojemność: {mem.Capacity / 1024 / 1024:.2f} MB")
+
+for disk in w.Win32_DiskDrive():
+    print("Dysk twardy:")
+    print(f"- Model: {disk.Model}")
+    print(f"- Pojemność: {disk.Size / 1024 / 1024 / 1024:.2f} GB")
